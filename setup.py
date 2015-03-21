6a472e343d478e2a895679305a632f1fe56f4f77
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2015-03-20 22:17:08 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='namegenerator',
+    version='1.0',
+    author='Mario Cesar Senoranis Ayala <mariocesar.c50@gmail.com>',
+    packages=find_packages(''),
+)
